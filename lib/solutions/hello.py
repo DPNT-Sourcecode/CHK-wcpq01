37607756863624c04fae152dd847dfe5a7b41bf9
sbx_user1060 <sbx_user1060@ip-10-11-110-7.eu-west-2.compute.internal>
2018-06-10 21:30:15 +0000

    Sun Jun 10 21:30:15 UTC 2018

--- lib/solutions/hello.py
+++ lib/solutions/hello.py
@@ -5,3 +5,8 @@
 def hello(friend_name):
     full_name = ' '.join([x.capitalize() for x in str(friend_name).split()])
     return "Hello, {}".format(full_name)
+
+
+# Testing
+if __name__ == '__main__':
+    print hello("Mr. x jackson")
