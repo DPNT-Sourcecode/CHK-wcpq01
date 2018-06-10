4deaa09bbadf29267e0f8addf673cd7292e6c941
sbx_user1060 <sbx_user1060@ip-10-11-110-7.eu-west-2.compute.internal>
2018-06-10 21:30:42 +0000

    Sun Jun 10 21:30:42 UTC 2018

--- lib/solutions/hello.py
+++ lib/solutions/hello.py
@@ -4,7 +4,7 @@
 # friend_name = unicode string
 def hello(friend_name):
     full_name = ' '.join([x.capitalize() for x in str(friend_name).split()])
-    return "Hello, {}".format(full_name)
+    return "Hello, {}!".format(full_name)
 
 
 # Testing
